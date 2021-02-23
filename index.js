507f7c49383d461c2b85dff2ae9b31b387b6cfa2
Ricardo <ricardo.br.pi@gmail.com>
2021-02-23 11:04:54 -0300

    style: add styling to the button

---
+++ index.js
@@ -0,0 +1,6 @@
+function Myfunction () {
+  console.log("Ola Mundo")
+  document.getElementById("btn").style.background = "linear-gradient(aqua, rgb(255, 105, 243))";
+  var element = document.getElementById("btn");
+  element.classList.add("mystyle");
+}
