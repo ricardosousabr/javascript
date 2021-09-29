a5d7ae82f2fde2859666d3b8d912c74d0d5f084c
ricardo <ricardo.br.pi@gmail.com>
2021-09-29 20:38:34 -0300

    fix btn function bug

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 function btn () {
   for (i = 0; i < array.length; ++i){
     var result = array[i] % 2;
-    if (i < 16) {
-      console.log(result)
+    if (result === 1) {
+      console.log(array[i])
     }
   }
 }
