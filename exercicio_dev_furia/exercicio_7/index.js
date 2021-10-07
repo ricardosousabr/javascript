0df05064d4c8e4b963314f3d01c0d844d89fb189
ricardo <ricardo.br.pi@gmail.com>
2021-10-07 00:01:07 -0300

    feat: add the 7th exercise

---
+++ exercicio_dev_furia/exercicio_7/index.js
@@ -0,0 +1,17 @@
+function somar(valorSomar) {
+  return valorSomar + valorSomar
+};
+
+function subtrair(valorSubtrair) {
+  return valorSubtrair - valorSubtrair
+};
+
+function dividir(valorDividir) {
+  return valorDividir / valorDividir
+};
+
+function multiplicar(valorMultiplicar) {
+  return valorMultiplicar * valorMultiplicar
+};
+
+console.log(somar(10), subtrair(5), dividir(50), multiplicar(10))
