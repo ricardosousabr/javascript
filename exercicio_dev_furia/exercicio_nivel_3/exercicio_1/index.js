bcb62a758201d69b8704c9f54b2201d0ea3929c9
ricardo <ricardo.br.pi@gmail.com>
2021-10-12 22:00:02 -0300

    feat: add the 1th exercise of level 3

---
+++ exercicio_dev_furia/exercicio_nivel_3/exercicio_1/index.js
@@ -0,0 +1,10 @@
+function numeroPrimo(numero) {
+  if (numero > 1 && numero / numero && numero / 1 ){
+    return "Número Primo"
+  }
+  else {
+    return "Não é número Primo"
+  };
+};
+
+console.log(numeroPrimo(4))
