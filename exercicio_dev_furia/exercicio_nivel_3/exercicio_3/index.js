6746f19231d1d151ea40821d654996e131ad9ba8
ricardo <ricardo.br.pi@gmail.com>
2021-10-15 22:04:06 -0300

    feat: add the 3th exercise of level 3

---
+++ exercicio_dev_furia/exercicio_nivel_3/exercicio_3/index.js
@@ -0,0 +1,9 @@
+function DividirNumeroDivisorMaximo(valor1, valor2) {
+  for(i = 0; i < valor1; i++){
+    if (valor1 % i === 0 && valor2 % i === 0) {
+      console.log(i)
+    }
+  }
+}
+
+return numeroDivisorMaximo(60, 10);
