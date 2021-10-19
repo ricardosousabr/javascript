eb4c4a5e284aa14f18664c0ab54c81933f24c8f7
ricardo <ricardo.br.pi@gmail.com>
2021-10-18 22:05:35 -0300

    refactor: refactor the function name

--- exercicio_dev_furia/exercicio_nivel_3/exercicio_3/index.js
+++ exercicio_dev_furia/exercicio_nivel_3/exercicio_3/index.js
@@ -1,4 +1,4 @@
-function DividirNumeroDivisorMaximo(valor1, valor2) {
+function numeroDivisorMaximo(valor1, valor2) {
   for(i = 0; i < valor1; i++){
     if (valor1 % i === 0 && valor2 % i === 0) {
       console.log(i)
