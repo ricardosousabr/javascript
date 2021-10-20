c12e511d5864bf2d6f66069fc752a2f0889fdd80
ricardo <ricardo.br.pi@gmail.com>
2021-10-19 22:44:57 -0300

    feat: Create Algorithm to Sum Array Items

---
+++ exercicio_dev_furia/exercicio_nivel_4/exercicio_2/index.js
@@ -0,0 +1,10 @@
+const array = [10, 10];
+
+function somarItensDeVetores() {
+  for (i = 1; i < array.length; i++){
+    let somar = array[0] + array[1];
+    console.log(somar)
+  }
+};
+
+return somarItensDeVetores();
