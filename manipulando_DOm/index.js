eeb6c0ee67fcccd1a73fcdae192fdece46baa02d
ricardo <ricardo.br.pi@gmail.com>
2021-11-03 22:59:38 -0300

    feat: Manipulate the DOM through javascript methods

---
+++ manipulando_DOm/index.js
@@ -0,0 +1,5 @@
+const localdohref = window.location.href
+const selecionarClass = document.querySelector(".ativo")
+const linguagemDoNavegdor = navigator.language
+const alturaDaPagina =  window.screen.height
+const larguraDaPagina = window.screen.width
