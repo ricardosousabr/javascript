8610e530f6cc8284c47b2547c17f3cdbdaceaace
ricardo <ricardo.br.pi@gmail.com>
2021-11-09 21:56:58 -0300

    feat: create function to enter text on screen

--- manipulando_DOm/eventos/index.js
+++ manipulando_DOm/eventos/index.js
@@ -2,5 +2,14 @@ const classBtn = document.body.querySelector(".btn")
 classBtn.addEventListener("click", changeBackground)
 
 function changeBackground() {
-  document.body.style.background = document.querySelector(".input_Text").value
+  document.body.style.background = document.querySelector(".input_Color").value
 }
+
+const titulo = document.body.querySelector(".title")
+const valor = document.querySelector(".input_Text").value
+const btn_Inserir = document.body.querySelector(".btn_Insertr_Text")
+
+function insertText(){
+}
+
+btn_Inserir.addEventListener("click", insertText)
