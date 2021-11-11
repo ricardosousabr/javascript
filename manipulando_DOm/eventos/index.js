018656f312055a5ef05faf800d592eb95ae61a8f
ricardo <ricardo.br.pi@gmail.com>
2021-11-10 21:53:51 -0300

    fix: Fix the insertText function

--- manipulando_DOm/eventos/index.js
+++ manipulando_DOm/eventos/index.js
@@ -6,10 +6,15 @@ function changeBackground() {
 }
 
 const titulo = document.body.querySelector(".title")
-const valor = document.querySelector(".input_Text").value
 const btn_Inserir = document.body.querySelector(".btn_Insertr_Text")
 
 function insertText(){
+  titulo.innerHTML = document.querySelector(".input_Text").value;
+}
+
+function clear(){
+  document.querySelector(".input_Text").value = "";
 }
 
 btn_Inserir.addEventListener("click", insertText)
+btn_Inserir.addEventListener("click", clear)
