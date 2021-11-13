f819fd3df6d9676c4322002a36cdc39284ae4ae1
ricardo <ricardo.br.pi@gmail.com>
2021-11-13 16:07:32 -0300

    feat: add an exist for the button and a styling for the two buttons

--- manipulando_DOm/eventos/index.js
+++ manipulando_DOm/eventos/index.js
@@ -1,20 +1,37 @@
 const classBtn = document.body.querySelector(".btn")
+const titulo = document.body.querySelector(".title")
+const btn_Inserir = document.body.querySelector(".btn_Insertr_Text")
+
+
 classBtn.addEventListener("click", changeBackground)
+
 
 function changeBackground() {
   document.body.style.background = document.querySelector(".input_Color").value
 }
 
-const titulo = document.body.querySelector(".title")
-const btn_Inserir = document.body.querySelector(".btn_Insertr_Text")
 
 function insertText(){
   titulo.innerHTML = document.querySelector(".input_Text").value;
 }
 
+btn_Inserir.addEventListener("click", insertText)
+
+
 function clear(){
   document.querySelector(".input_Text").value = "";
 }
 
-btn_Inserir.addEventListener("click", insertText)
 btn_Inserir.addEventListener("click", clear)
+
+function moveBtn(){
+  classBtn.classList.add("btn_hover")
+}
+
+function backBtn(){
+  classBtn.classList.remove("btn_hover")
+}
+
+
+classBtn.addEventListener("mouseover", moveBtn)
+classBtn.addEventListener("mouseout", backBtn)
